acbd2f83b40d8dbe118fd2eb9f1c2f919663c8d3
Andrew Fray <andrew@spryfox.com>
2013-07-28 09:37:49 +0100

    removed unnecessary reference to unityengine

--- Assets/UnTest/Editor/Tests/AssertFacts.cs
+++ Assets/UnTest/Editor/Tests/AssertFacts.cs
@@ -8,7 +8,6 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-using UnityEngine;
 using UnTest;
 using System;
 
