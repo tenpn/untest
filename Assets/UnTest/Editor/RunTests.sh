c779401d29f35a04771d26dc1a7bb7b4ad736c9c
Andrew Fray <andrew@spryfox.com>
2013-07-29 12:18:14 +0100

    a script that builds using mdtool then runs tests using untest-console

---
+++ Assets/UnTest/Editor/RunTests.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# builds solution, then runs tests. 
+# execute with RunTests.sh path/to/myunity.sln
+
+set -e
+
+/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool build $1
+
+SLNDIR=$(dirname $1)
+mono $SLNDIR/Assets/UnTest/Editor/UnTest-Console/bin/Release/UnTest-Console.exe $SLNDIR/Temp/bin/Debug
+
