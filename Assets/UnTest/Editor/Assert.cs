30f0dc9f53dc55d358126eaf7724265c2d52236c
tenpn <andrew.fray@gmail.com>
2013-08-03 16:59:55 +0100

    more informative assert if Assert.IsEqualSequence fails
    fixes #12

--- Assets/UnTest/Editor/Assert.cs
+++ Assets/UnTest/Editor/Assert.cs
@@ -63,7 +63,36 @@ public static class Assert {
             return;
         }
 
-        throw new Exception("Unequal sequences");
+        var lhsEnumerator = lhs == null ? null : lhs.GetEnumerator();
+        var rhsEnumerator = rhs == null ? null : rhs.GetEnumerator();
+
+        var comp = new System.Text.StringBuilder();
+
+        comp.Append("Sequences not equal\n");
+        comp.AppendFormat("{0} elements in left-hand side, {1} elements in right\n",
+                          (lhs == null ? -1 : lhs.Count()), 
+                          (rhs == null ? -1 : rhs.Count()));
+        comp.Append("LHS | RHS\n");
+
+        do
+        {
+            if (lhsEnumerator != null && lhsEnumerator.MoveNext() == false) {
+                lhsEnumerator = null;
+            }
+            if (rhsEnumerator != null && rhsEnumerator.MoveNext() == false) {
+                rhsEnumerator = null;
+            }
+
+            var lhsValue = lhsEnumerator == null ? " " : lhsEnumerator.Current.ToString();
+            var rhsValue = rhsEnumerator == null ? " " : rhsEnumerator.Current.ToString();
+            
+            comp.AppendFormat("{0} | {1}\n", lhsValue, rhsValue);
+            
+        } while(lhsEnumerator != null || rhsEnumerator != null);
+        
+
+        throw new Exception(comp.ToString());
+
     }
 }
 
